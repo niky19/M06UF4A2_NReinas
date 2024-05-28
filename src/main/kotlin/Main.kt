473632ec87402f3c5c70818f4202984f8459dd18
niky <nikita.barbosa.7e5@itb.cat>
2024-05-28 10:17:12 +0200

    commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,33 +1,125 @@
+import java.util.*
+import kotlin.system.measureTimeMillis
+
 fun main() {
-    val nReinas = NReinas()
-    val n = 8
-    nReinas.buscarSoluciones(n)
-    println("Número de soluciones: ${nReinas.numSoluciones()}")
-    for (i in 0 until nReinas.numSoluciones()) {
-        println("Solución ${i + 1}:")
-        nReinas.visualizaSolucion(i).forEach { fila ->
-            println(fila)
+    val sc = Scanner(System.`in`)
+    var midaTauler: Int? = null
+    var tempsTranscorregut: Long = 0
+
+    while (midaTauler == null) {
+        println("Introdueix la mida del tauler:")
+
+        if (sc.hasNextInt()) {
+            midaTauler = sc.nextInt()
+            if (midaTauler <= 0) {
+                println("Has introduït un número negatiu.")
+                midaTauler = null
+            }
+        } else {
+            sc.next()
+            println("Entrada no vàlida, si us plau, introdueix un número.")
         }
-        println()
     }
+
+    val reines = SolucionadorReines()
+
+    var opcioUsuari: Int? = null
+
+    while (opcioUsuari == null) {
+        println("Vols veure totes les solucions o només una?")
+        println(
+            "1. Totes les solucions\n" +
+                    "2. Una solució"
+        )
+        if (sc.hasNextInt()) {
+            opcioUsuari = sc.nextInt()
+            if (opcioUsuari !in 1..2) {
+                println("Número fora de rang.")
+                opcioUsuari = null
+            }
+        } else {
+            sc.next()
+            println("Entrada no vàlida, si us plau, introdueix 1 o 2.")
+        }
+    }
+
+    tempsTranscorregut = measureTimeMillis {
+        when (opcioUsuari) {
+            1 -> reines.buscaTotesSolucions(midaTauler)
+            2 -> reines.buscaSolucio(midaTauler, true)
+        }
+    }
+
+    val numSolucions = reines.comptaSolucions()
+    println("Nombre de solucions trobades: $numSolucions")
+
+    var solucioMostrada: Int? = null
+
+    while (solucioMostrada == null) {
+        println("Introdueix el número de la solució que vols veure:")
+        if (sc.hasNextInt()) {
+            solucioMostrada = sc.nextInt()
+            if (solucioMostrada !in 1..numSolucions) {
+                println("Número fora de rang.")
+                solucioMostrada = null
+            }
+        } else {
+            sc.next()
+            println("Entrada no vàlida, si us plau, introdueix un número.")
+        }
+    }
+
+    println(reines.mostraSolucio(solucioMostrada) + "\nTemps transcorregut: $tempsTranscorregut ms")
 }
 
-class NReinas {
-    private var soluciones: MutableList<List<Int>> = mutableListOf()
-    private var columna: MutableList<Int> = mutableListOf()
+class SolucionadorReines {
+    private lateinit var solucions: MutableList<String>
+    private var numSolucions = 0
+    private var fila = 0
+    private lateinit var columna: IntArray
+    private var trobaUna = false
+    private var midaTauler = 0
 
-    fun buscarSoluciones(n: Int) {
-        soluciones.clear()
-        columna = MutableList(n + 1) { 0 }
-        var fila = 1
-        columna[fila] = 0
+    fun buscaSolucio(midaTauler: Int, trobaUna: Boolean) {
+        reinicia()
+        this.numSolucions = 0
+        this.solucions = mutableListOf()
+        this.columna = IntArray(midaTauler + 1)
+        this.fila = 1
+        this.columna[fila] = 0
+        this.trobaUna = trobaUna
+        this.midaTauler = midaTauler
 
+        resol()
+    }
+
+    fun buscaTotesSolucions(midaTauler: Int) {
+        reinicia()
+        this.numSolucions = 0
+        this.solucions = mutableListOf()
+        this.columna = IntArray(midaTauler + 1)
+        this.fila = 1
+        this.columna[fila] = 0
+        this.midaTauler = midaTauler
+
+        resol()
+    }
+
+    fun comptaSolucions(): Int {
+        return this.numSolucions
+    }
+
+    fun mostraSolucio(numeroSolucio: Int): String? {
+        return if (numeroSolucio != 0 && this.solucions.isNotEmpty()) this.solucions[numeroSolucio - 1] else null
+    }
+
+    private fun resol() {
         while (fila > 0) {
-            if (columna[fila] < n) {
+            if (columna[fila] < midaTauler) {
                 columna[fila] += 1
-                if (esValido(fila)) {
-                    if (fila == n) {
-                        agregarSolucion()
+                if (esNodeValid(fila, columna)) {
+                    if (fila == midaTauler) {
+                        tractaSolucio(columna)
                     } else {
                         fila += 1
                         columna[fila] = 0
@@ -39,30 +131,30 @@ class NReinas {
         }
     }
 
-    fun numSoluciones(): Int {
-        return soluciones.size
-    }
-
-    fun visualizaSolucion(numeroSolucion: Int): List<String> {
-        val solucion = soluciones[numeroSolucion]
-        val n = solucion.size
-        val tablero = MutableList(n) { MutableList(n) { '.' } }
-        for (i in solucion.indices) {
-            tablero[i][solucion[i] - 1] = 'Q'
-        }
-        return tablero.map { it.joinToString("") } // Imprime el tablero en string separado por espacios
-    }
-
-    private fun esValido(fila: Int): Boolean {
+    private fun esNodeValid(fila: Int, columna: IntArray): Boolean {
         for (f in 1 until fila) {
-            if (columna[f] == columna[fila] || (f - columna[f] == fila - columna[fila]) || (f + columna[f] == fila + columna[fila])) {
+            if (columna[f] == columna[fila] || f - columna[f] == fila - columna[fila] || f + columna[f] == fila + columna[fila]) {
                 return false
             }
         }
         return true
     }
 
-    private fun agregarSolucion() {
-        soluciones.add(columna.subList(1, columna.size))
+    private fun tractaSolucio(columna: IntArray) {
+        var solucio = ""
+        for (f in 1..midaTauler) {
+            solucio += "Reina $f / fila $f / columna${columna[f]}\n"
+        }
+        this.solucions.add(solucio)
+        this.numSolucions++
+
+        if (this.trobaUna) {
+            this.trobaUna = false
+        }
+    }
+
+    private fun reinicia() {
+        this.fila = 0
+        this.numSolucions = 0
     }
 }
